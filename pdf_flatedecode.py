94f713d18ddf7f3819aa618d6c00460de5b70bcc
esack7 <isaacheist@gmail.com>
2018-01-03 11:34:35 -0800

    all work i did in last few days, not able to decode stream because of 'header' issue.

--- pdf_flatedecode.py
+++ pdf_flatedecode.py
@@ -2,14 +2,14 @@
 import re
 import zlib
 
-pdf = open("./files/Beautiful - G.pdf", "r+b").read()
+pdf = open("./files/test.pdf", "r+b").read()
 stream = re.compile(r'.*?FlateDecode.*?stream(.*?)endstream', re.S)
-print(stream)
 
 for s in stream.findall(pdf):
+    print(s)
     s = s.strip('\r\n')
     try:
-        f= open("newPDF.pdf","w+")
+        f= open("unzippedPDF.pdf","w+")
         f.write(zlib.decompress(s))
         f.close()
         print("")
